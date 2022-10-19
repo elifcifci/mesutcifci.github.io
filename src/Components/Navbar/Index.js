9d3911d09636fc07921d24b3f1ef99567b6307c5
Elif Çiftçi <eliffbat@gmail.com>
2022-10-19 23:08:18 +0300

    project deployed

--- src/Components/Navbar/Index.js
+++ src/Components/Navbar/Index.js
@@ -2,17 +2,17 @@ import * as React from "react";
 import { useRef } from "react";
 import { useCycle } from "framer-motion";
 import { NavbarButton } from "../NavbarButton";
-import { Navigation } from "../Navigation";
+import { Navigation } from "../Navigation/iIndex";
 import { Container, Sidebar, sidebarAnimationConfigs } from "./styles";
 
- const Navbar = () => {
+const Navbar = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
   const containerRef = useRef(null);
 
   return (
     <Container
       initial={false}
-      animate={isOpen ? "open" : "closed"}  
+      animate={isOpen ? "open" : "closed"}
       ref={containerRef}
     >
       <Sidebar variants={sidebarAnimationConfigs} />
